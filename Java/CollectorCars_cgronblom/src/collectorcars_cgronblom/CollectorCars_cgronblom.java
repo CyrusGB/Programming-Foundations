e4d42f45a04675a4835e060e335c2b4d8f77eded
Cyrus <cjgronblom@gmail.com>
2024-11-07 14:34:35 -0500

    Added new collector cars proj

---
+++ Java/CollectorCars_cgronblom/src/collectorcars_cgronblom/CollectorCars_cgronblom.java
@@ -0,0 +1,27 @@
+/************************
+ * Title:
+ * Date:
+ * Author: Cyrus Gronblom
+ * Purpose:
+ * **********************
+ */
+
+package collectorcars_cgronblom;
+
+public class CollectorCars_cgronblom {
+    // Driver method, main
+    public static void main(String[] args) {
+        
+        // Create new instances of title and go again
+        Title title = new Title();
+        GoAgain goAgain = new GoAgain();
+        
+        // Print Title
+        title.printTitle("Collector Cars", "Cars I guess");
+
+        // Start the loop and ask user if they want to go again after
+        goAgain.goAgain();
+
+    } // END MAIN
+
+} // END CLASS
---
+++ Java/CollectorCars_cgronblom/src/collectorcars_cgronblom/GoAgain.java
@@ -0,0 +1,52 @@
+/************************
+ * Title: Go Again
+ * Date: 9/10/24
+ * Author: Cyrus Gronblom
+ * Purpose: Demonstrates how we can use while loops in future projects.
+ * **********************
+ */
+
+package collectorcars_cgronblom;
+
+import java.util.Scanner;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class GoAgain {
+    
+    // Ask user if they want to continue the loop
+    public void goAgain(){
+        
+        // Create new instance of scanner class for user input
+        Scanner userInput = new Scanner(System.in);
+        
+        // String to hold user input (y/n)
+        String strkeepGoing = "y";
+        
+        // While loop
+        while(strkeepGoing.equalsIgnoreCase("y") || strkeepGoing.equalsIgnoreCase("yes")){
+            // Debugging
+            //System.err.println("Inside the while loop");
+            
+            // Add Source code here
+            // --------------------
+            
+            
+            
+            // --------------------
+            // End Source Code
+            
+            // Ask user to run again
+            System.out.print("\nWould you like to run again?: ");
+            // Read user input
+            strkeepGoing = userInput.next();
+            System.out.println("");
+        }
+
+        // Close user input to resolve resource leak
+        userInput.close();
+
+    } // END goAgain()
+    
+} // END CLASS
